7f99857e9c2fd7612d4239956769751fe3ff0a47
Dzmitry Bladyka <dzmitrydistortion@gmail.com>
2023-02-22 11:12:18 +0300

    Completed

---
+++ H4XOR News/Views/WebView.swift
@@ -0,0 +1,29 @@
+//
+//  WebView.swift
+//  H4XOR News
+//
+//  Created by Dzmitry Bladyka on 22.02.23.
+//
+
+import Foundation
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+}
